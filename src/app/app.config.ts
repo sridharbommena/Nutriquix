d48d673ed8e040e32ff9e42df6294054bd59166a
Sridhar Bommena <sridharbommena520@gmail.com>
2025-07-04 13:28:53 +0530

    added firebase to the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { firebaseConfig } from '../environment/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(()=> initializeApp(firebaseConfig)),
+    provideAuth(()=> getAuth()),
   ]
 };
