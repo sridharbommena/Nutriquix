2bfe1f250a1139799dea6183476cd970b6118011
Sridhar Bommena <sridharbommena520@gmail.com>
2025-07-05 10:01:31 +0530

    added project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Landing } from './components/landing/landing';
+import { Register } from './components/register/register';
+import { Login } from './components/login/login';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Landing
+    },
+    {
+        path: "register",
+        component: Register
+    },
+    {
+        path: "login",
+        component: Login
+    },
+    {
+        path: "home",
+        component: Home,
+        // canActivate: //TODO: Add a AuthGuard
+    }
+];
