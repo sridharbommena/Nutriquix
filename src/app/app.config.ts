e18926f348234eb700492ceff9d3d414fcbac3e6
Sridhar Bommena <sridharbommena520@gmail.com>
2025-07-05 14:08:32 +0530

    added configuration for secure deployment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,17 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { firebaseConfig } from '../environment/environment';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(()=> initializeApp(firebaseConfig)),
+    provideFirebaseApp(()=> initializeApp(environment.firebaseConfig)),
     provideAuth(()=> getAuth()),
     provideAnimationsAsync(),
         providePrimeNG({
--- src/environment/environment.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBPuewU-q73g9ouQl5a21eezOGiWRlfRUI",
-    authDomain: "giftedchat-7ccf1.firebaseapp.com",
-    databaseURL: "https://giftedchat-7ccf1.firebaseio.com",
-    projectId: "giftedchat-7ccf1",
-    storageBucket: "giftedchat-7ccf1.firebasestorage.app",
-    messagingSenderId: "122015404466",
-    appId: "1:122015404466:web:d614a2593d3cf329e414ab"
-};
