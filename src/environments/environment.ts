27414655e01c994bbd11740369c51d6712bab4bc
Sridhar Bommena <sridharbommena520@gmail.com>
2025-07-08 21:46:51 +0530

    added local setup scripts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "undefined",
-    authDomain: "undefined",
-    projectId: "undefined",
-    storageBucket: "undefined",
-    messagingSenderId: "undefined",
-    appId: "undefined"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
